e29059820d32219e0ee36f20df18cb520bbc09c7
dysady <dysadgame1@gmail.com>
2025-01-28 20:12:24 +0100

    gestion des comptes fonctionnel

--- web_serveur/scripts/postgresqlService.js
+++ web_serveur/scripts/postgresqlService.js
@@ -56,6 +56,52 @@ class PostgresqlService {
             client.release(); // Libérer le client pour le retourner au pool
         }
     }
+
+    // Créer un nouvel utilisateur
+    async createUser(user) {
+        const client = await this.connect();
+        try {
+            const query = `
+                INSERT INTO library._Users 
+                (name, age, passwords, child, familial_situation, gender, cat_socio_pro, lieu_habitation, frequency, book_size, birth_date)
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
+                RETURNING user_id;
+            `;
+            const values = [
+                user.username,
+                user.age,
+                user.password,
+                user.child,
+                user.familial_situation,
+                user.gender,
+                user.cat_socio_pro,
+                user.lieu_habitation,
+                user.frequency,
+                user.book_size,
+                user.birth_date,
+            ];
+            const res = await client.query(query, values);
+            return res.rows[0]; // Retourne l'ID du nouvel utilisateur
+        } finally {
+            client.release();
+        }
+    }
+
+    // Authentifier un utilisateur
+    async authenticateUser(name, passwords) {
+        const client = await this.connect();
+        try {
+            const query = `
+                SELECT * FROM library._Users
+                WHERE name = $1 AND passwords = $2;
+            `;
+            const values = [name, passwords];
+            const res = await client.query(query, values);
+            return res.rows.length > 0 ? res.rows[0] : null; // Renvoie l'utilisateur ou null
+        } finally {
+            client.release();
+        }
+    }
     
 }
 
