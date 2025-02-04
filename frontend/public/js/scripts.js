fa1b224824656b219665ced9a6f4ea1fd0978100
dysad <dysadgame1@gmail.com>
2025-02-04 20:57:44 +0000

    21:57

--- frontend/public/js/scripts.js
+++
@@ -1,6 +0,0 @@
-// Exemple d'interaction JS (ajoutez des fonctionnalités au besoin)
-
-// Par exemple, afficher un message lors d'un clic sur Connexion
-document.querySelector('button').addEventListener('click', () => {
-    alert('Connexion non implémentée !');
-});
