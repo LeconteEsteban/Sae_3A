8e87e13e0c1a30fd0a178956e1b596986e463e24
dysad <dysadgame1@gmail.com>
2025-02-28 14:54:50 +0000

    Com ascii

--- python/service/traitement_ascii.py
+++ python/service/traitement_ascii.py
@@ -3,11 +3,11 @@ import pandas as pd
 import re
 import ftfy
 
-Fonction pour détecter des caractères non-ASCII
+#Fonction pour détecter des caractères non-ASCII
 def contains_non_ascii(text):
     return bool(re.search(r'[^\x00-\x7F]', str(text)))
 
-Fonction pour ré-encoder les titres en UTF-8
+#Fonction pour ré-encoder les titres en UTF-8
 def reencode_to_utf8(text):
     try:
         # Essayer de décoder en UTF-8 directement
@@ -19,27 +19,27 @@ def reencode_to_utf8(text):
         except (UnicodeEncodeError, UnicodeDecodeError):
             return text
 
-Fonction pour corriger les erreurs d'encodage avec ftfy
+#Fonction pour corriger les erreurs d'encodage avec ftfy
 def fix_encoding(text):
     return ftfy.fix_text(text)
 
-Lire le fichier CSV
+#Lire le fichier CSV
 df = pd.read_csv('../default_data/bigboss_book.csv')
 
-Colonnes à traiter
+#Colonnes à traiter
 columns_to_fix = ['title', 'publisher', 'series', 'author', 'original_title', 'genre_and_votes', 'settings', 'characters', 'awards', 'description']
 
-Appliquer les fonctions sur les colonnes spécifiées
+#Appliquer les fonctions sur les colonnes spécifiées
 for column in columns_to_fix:
     df_with_non_ascii = df[df[column].apply(contains_non_ascii)]
     df_with_non_ascii[column] = df_with_non_ascii[column].apply(reencode_to_utf8)
     df_with_non_ascii[column] = df_with_non_ascii[column].apply(fix_encoding)
     df.update(df_with_non_ascii)
 
-Sauvegarder le fichier CSV corrigé
+#Sauvegarder le fichier CSV corrigé
 df.to_csv('../new_data/books_corrected.csv', index=False)
 
-Afficher les lignes contenant des caractères non-ASCII pour chaque colonne
+#Afficher les lignes contenant des caractères non-ASCII pour chaque colonne
 for column in columns_to_fix:
     print(f"Non-ASCII characters in column '{column}':")
     print(df[df[column].apply(contains_non_ascii)][column])
