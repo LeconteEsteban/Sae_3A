4298b948a9183da80e309c10fff557cea4b31493
momozhok <moh4med.0.ali@gmail.com>
2025-03-09 16:18:08 +0100

    chnagement mini

--- frontend/public/js/carousel.js
+++ frontend/public/js/carousel.js
@@ -1,5 +1,13 @@
 import { truncateDescription } from './utils.js';
 
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) {
+      return decodeURIComponent(parts.pop().split(';').shift());
+  }
+  return null;
+}
 
 function getIdAccount() {
   const userCookie = getCookie('user');
--- frontend/public/js/popup_list.js
+++ frontend/public/js/popup_list.js
@@ -1,6 +1,22 @@
 import { truncateDescription } from './utils.js';
 
-let currentSimilarBooks = [];
+
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) {
+      return decodeURIComponent(parts.pop().split(';').shift());
+  }
+  return null;
+}
+
+function getIdAccount() {
+  const userCookie = getCookie('user');
+  if (userCookie) {
+      const user = JSON.parse(userCookie);
+      return user;
+  }
+}
 
 async function fetchSimilarBooks(bookId, nBooks = 5) {
   try {
